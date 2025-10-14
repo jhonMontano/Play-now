22917226cf59231f6b107839261bde7552100806
jhonMontano <jhon.montano@correo.tdea.edu.co>
2025-10-13 22:04:33 -0500

    It´s created user register with validations

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,13 @@
-import express from "express";
+import app from "./src/app.js";
 import sequelize from "./src/config/database.js";
 
-const app = express();
 const PORT = process.env.PORT || 4000;
-
-app.get("/", (req, res) => {
-  res.send("Servidor PlayNow ");
-});
 
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
   try {
     await sequelize.sync();
-    console.log("Modelos sincronizados con la base de datos");
+    console.log("Modelos sincronizados con la base de datos PostgreSQL");
   } catch (error) {
     console.error("Error al sincronizar modelos:", error);
   }
