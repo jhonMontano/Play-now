7fbac11c8fafe2be6fe585e4ca9f5ea3e729983d
jhonMontano <jhon.montano@correo.tdea.edu.co>
2025-10-24 12:42:48 -0500

    The cors and messages are configured in Spanish

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 export const authenticateToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Token not provided" });
+  if (!token) return res.status(401).json({ message: "Token no proporcionado" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(403).json({ message: "Invalid or expired token" });
+    res.status(403).json({ message: "Token invalido o expirado" });
   }
 };
