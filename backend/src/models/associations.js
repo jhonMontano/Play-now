5446a7100dd4dd2836147cd52975e356f5c63118
jhonMontano <jhon.montano@correo.tdea.edu.co>
2025-11-01 21:41:39 -0500

    Modify cuourt

--- backend/src/models/associations.js
+++ backend/src/models/associations.js
@@ -4,15 +4,12 @@ import Mall from "./mall.js";
 import Court from "./court.js";
 
 Roles.hasMany(User, { foreignKey: "idRol", as: "usuarios" });
-
 User.belongsTo(Roles, { foreignKey: "idRol", as: "rol" });
 
 Mall.belongsTo(User, { foreignKey: "adminId", as: "administrador" });
-
 User.hasOne(Mall, { foreignKey: "adminId", as: "mallAdministrado" });
 
 Mall.hasMany(Court, { foreignKey: "mallId", as: "canchas" });
-
 Court.belongsTo(Mall, { foreignKey: "mallId", as: "mall" });
 
 export { Roles, User, Mall, Court };
