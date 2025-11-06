48259d853d77a3dc98bc35b1be035f98f4c820a0
jhonMontano <jhon.montano@correo.tdea.edu.co>
2025-11-05 21:08:13 -0500

    The column to activate or deactivate the user is created, and the logic in the user section is corrected

--- backend/src/services/user.js
+++ backend/src/services/user.js
@@ -1,20 +1,20 @@
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
-import nodemailer from "nodemailer";
-
+import { sendResetPasswordEmail } from "../utils/emailTemplats.js";
+import { createTransporter } from "../utils/mailer.js";
 
 export const registerUser = async (data) => {
   const { tipoDocumento, numeroDocumento, correo } = data;
 
   const existingUser = await User.findOne({ where: { numeroDocumento } });
-  if (existingUser) throw new Error("El documento ya esta registrado");
+  if (existingUser) throw new Error("El documento ya está registrado");
 
   const existingEmail = await User.findOne({ where: { correo } });
   if (existingEmail) throw new Error("El correo ya se encuentra registrado");
 
   return await User.create({
     ...data,
-    password: numeroDocumento, 
+    password: numeroDocumento,
   });
 };
 
@@ -32,6 +32,10 @@ export const updateUser = async (id, data) => {
   const user = await User.findByPk(id);
   if (!user) throw new Error("Usuario no encontrado");
 
+  if (data.hasOwnProperty("activo") && user.idMall) {
+    throw new Error("No se puede modificar el estado 'activo' de un usuario con centro comercial asignado.");
+  }
+
   await user.update(data);
   return user;
 };
@@ -41,9 +45,8 @@ export const deleteUser = async (id) => {
   if (!user) throw new Error("Usuario no encontrado");
 
   await user.destroy();
-  return { message: "Usuario eliminado exitosamente"};
+  return { message: "Usuario eliminado exitosamente" };
 };
-
 
 export const sendPasswordResetEmail = async (email) => {
   const user = await User.findOne({ where: { correo: email } });
@@ -51,39 +54,10 @@ export const sendPasswordResetEmail = async (email) => {
 
   const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
-  const resetLink = `http://localhost:8080/users/reset-password/${token}`;
+  const resetLink = `${process.env.FRONTEND_URL}/users/reset-password/${token}`;
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-});
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Recuperación de contraseña",
-    
-html: `
-  <div style="font-family: Arial, sans-serif; padding: 20px; background-color: #f9f9f9; border-radius: 8px; max-width: 500px; margin: auto;">
-  <h2 style="color: #333;">Hola 👋</h2>
-  
-  <p style="font-size: 16px; color: #555; line-height: 1.5;">
-    Recibimos una solicitud para restablecer tu contraseña. Haz clic en el botón de abajo para continuar.
-  </p>
-  
-  <a href="${resetLink}" 
-     style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #007bff; color: #ffffff; text-decoration: none; border-radius: 5px; font-weight: bold;">
-    Restablecer contraseña
-  </a>
-  
-  <p style="font-size: 14px; color: #777; margin-top: 20px; line-height: 1.5;">
-    Este enlace expirará en 1 hora. Si no solicitaste este cambio, puedes ignorar este mensaje.
-  </p>
-</div>
-`,
-  };
+  const transporter = createTransporter(); 
+  const mailOptions = sendResetPasswordEmail(email, resetLink);
 
   await transporter.sendMail(mailOptions);
 };
