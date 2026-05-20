77d66888bbf90e364684e2383165b3d918e86992
jhonMontano <jhon.montano@correo.tdea.edu.co>
2026-05-20 00:13:25 -0500

    Change in nodemailer

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+/*import nodemailer from "nodemailer";
 
 export const createTransporter = () => {
   const transporter = nodemailer.createTransport({
@@ -14,6 +14,36 @@ export const createTransporter = () => {
   }).catch((error) => {
     console.error("SMTP ERROR: no se pudo verificar el transporte", error);
   });
+
+  return transporter;
+};*/
+
+import nodemailer from "nodemailer";
+
+export const createTransporter = () => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "admonplaynow@gmail.com",
+      pass: "haygsyqysncjylgt",
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+    connectionTimeout: 15000,
+    greetingTimeout: 15000,
+    socketTimeout: 15000,
+  });
+
+  transporter.verify()
+    .then(() => {
+      console.log("SMTP OK");
+    })
+    .catch((error) => {
+      console.error("SMTP ERROR", error);
+    });
 
   return transporter;
 };
