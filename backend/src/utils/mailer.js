cf66dea78d34df32e00f1371fce4e356fb62729f
jhonMontano <jhon.montano@correo.tdea.edu.co>
2026-05-19 23:24:15 -0500

    It´s Modyfied templates

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -4,8 +4,8 @@ export const createTransporter = () => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: 'admonplaynow@gmail.com', //process.env.EMAIL_USER,
+      pass: 'haygsyqysncjylgt', //process.env.EMAIL_PASS,
     },
   });
 
